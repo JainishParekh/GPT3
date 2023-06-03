4288f9ca0d56f1ac08ecac63e1c22d91ffc1cb4a
JainishParekh <jainishparekh21@gmail.com>
2023-06-03 11:54:28 +0530

    Updates

---
+++ precache-manifest.865161d540dd84a93d2b4fe131be81e6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "df32927c282b58cf083b",
+    "url": "/GPT3/static/css/main.b06ea55c.chunk.css"
+  },
+  {
+    "revision": "df32927c282b58cf083b",
+    "url": "/GPT3/static/js/main.df32927c.chunk.js"
+  },
+  {
+    "revision": "d66cbf447e233c8ad613",
+    "url": "/GPT3/static/js/1.d66cbf44.chunk.js"
+  },
+  {
+    "revision": "f5a3d14970b5d58ce3fc",
+    "url": "/GPT3/static/js/runtime~main.f5a3d149.js"
+  },
+  {
+    "revision": "a95f42a40ce776c07a2891c220f620ae",
+    "url": "/GPT3/static/media/logo.a95f42a4.svg"
+  },
+  {
+    "revision": "fea810ed22f6fc37fab045129a01ed07",
+    "url": "/GPT3/static/media/blog01.fea810ed.png"
+  },
+  {
+    "revision": "e76b2c16b1e4ccf994a24eda75591951",
+    "url": "/GPT3/static/media/blog02.e76b2c16.png"
+  },
+  {
+    "revision": "404d1a405e0cb0936f224fcd374c6cb0",
+    "url": "/GPT3/static/media/blog03.404d1a40.png"
+  },
+  {
+    "revision": "d682b195d844afb2a4ce1e8573602221",
+    "url": "/GPT3/static/media/blog04.d682b195.png"
+  },
+  {
+    "revision": "576efe185f7f571f39d782fde1d28fa9",
+    "url": "/GPT3/static/media/blog05.576efe18.png"
+  },
+  {
+    "revision": "3d84899326ac5f0f66149fa037693d18",
+    "url": "/GPT3/static/media/people.3d848993.png"
+  },
+  {
+    "revision": "17ade55739aedbbb39e78442cdad6fb3",
+    "url": "/GPT3/static/media/ai.17ade557.png"
+  },
+  {
+    "revision": "3c84ae2642b58c3bc28340a7e763fc23",
+    "url": "/GPT3/static/media/possibility.3c84ae26.png"
+  },
+  {
+    "revision": "1313fefc34e8bbfbfab1c29f27f01c32",
+    "url": "/GPT3/index.html"
+  }
+];
